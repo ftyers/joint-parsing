6674febbfa924ef273f7bf2b24a6569375bd30b3
sereni <sereni.nm@gmail.com>
2016-04-04 09:38:12 +0300

    Add dependencies

--- parser/morph-analyser.py
+++ parser/morph-analyser.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
+# cat data/en-ud-test.conllu | python3 analyser.py data/english.dict
 
-import sys;
+import sys
 
-lookup = {};
+lookup = {}
 
-for line in open(sys.argv[1]).readlines(): #{
-	if line.strip() == '': #{
-		continue;
-	#}
-	row = line.strip().split('\t'); 
-#	print(row);
-	if row[0] not in lookup: #{
-		lookup[row[0]] = set();
-	#}
-	lookup[row[0]].add((row[1], row[2], row[3]));
-#}
+for line in open(sys.argv[1]).readlines():  # {
+    if line.strip() == '':  # {
+        continue;
+    # }
+    row = line.strip().split('\t')
+    #	print(row);
+    if row[0] not in lookup:  # {
+        lookup[row[0]] = set()
+    # }
+    lookup[row[0]].add((row[1], row[2], row[3]))
+# }
 
-for line in sys.stdin.readlines(): #{
+for line in sys.stdin.readlines():  # {
 
-	if line.strip() == '': #{
-		sys.stdout.write(line);
-		continue;
-	#}
-	row = line.split('\t');
+    if line.strip() == '':  # {
+        sys.stdout.write(line)
+        continue;
+    # }
+    row = line.split('\t')
 
-	num = int(row[0]);
-	sur = row[1];
-	lem = row[2];
-	cat = row[4];
-	cha = row[5];
+    num = int(row[0])
+    sur = row[1]
+    lem = row[2]
+    cat = row[4]
+    cha = row[5]
 
-	# 1	Al	Al	PROPN	NNP	Number=Sing	3	name	_	SpaceAfter=No
-	if sur in lookup: #{
-		for analys in lookup[sur]: #{
-			print('%d\t%s\t%s\t%s\t_\t%s' % (num, sur, analys[0], analys[1], analys[2]));
-		#}
-	else: #{
-		print('WARNING: "%s" not found in dictionary.', file=sys.stderr);
-		print('%d\t%s\t%s\t%s\t_\t%s' % (num, sur, lem, cat, cha));
-	#}
-#}
+    # 1	Al	Al	PROPN	NNP	Number=Sing	3	name	_	SpaceAfter=No
+    if sur in lookup:  # {
+        for analys in lookup[sur]:  # {
+            print('%d\t%s\t%s\t%s\t_\t%s' % (num, sur, analys[0], analys[1], analys[2]));
+        # }
+    else:  # {
+        print('WARNING: "%s" not found in dictionary.', file=sys.stderr);
+        print('%d\t%s\t%s\t%s\t_\t%s' % (num, sur, lem, cat, cha));
+    # }
+# }
